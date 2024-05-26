d79fe6095257e8e1b9656b7f5e033f34bc84b55c
Sergio <sergio.ume@hotmail.com>
2024-05-26 19:47:10 +0100

    videos 5 y 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pildoras';
+  title = 'sergio';
+
+  saludo= 'Buenos días';
 }
