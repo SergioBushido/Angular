05cb911e655c2435789546b948e735cba62b58e9
Sergio <sergio.ume@hotmail.com>
2024-05-27 08:29:50 +0100

    interpolacion y funciones

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class EmpleadoComponent {
 
+
+  nombre ="sergio";
+  apellidos ="fernandez";
+  private edad =8;
+  //empresa ="smj";
+
+
+  getEdad(){
+    return this.edad>=18 ? "mayor de edad":"menor de edad"}
+
+  llamaEmpresa(value:String){}
+  
 }
