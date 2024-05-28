6f248497282a7687330ba9d0a12dbeabb9867af8
Sergio <sergio.ume@hotmail.com>
2024-05-28 09:37:45 +0100

    esta version tiene bootstrap

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -11,12 +11,33 @@ export class EmpleadoComponent {
   nombre ="sergio";
   apellidos ="fernandez";
   private edad =8;
-  //empresa ="smj";
+  empresa ="smj";
+ 
 
+  habilitacionCuadro=true;
+  usuRegistrado=false;
+  textoDeRegistro="no hay nadie registrado";
+
+  getRegistroUsuario(){
+    this.usuRegistrado=false
+  }
 
   getEdad(){
     return this.edad>=18 ? "mayor de edad":"menor de edad"}
 
   llamaEmpresa(value:String){}
+
+  //$event indica dentro del parentesis de la funcion que hay un evento...ese evento es de tipo HTMLInputElement y puede recuperar los "values" declarados
+  setUsuarioRegistrado(even:Event){
+   // alert("Usuario Registrado")
+   if((<HTMLInputElement>event?.target).value=="si"){
+    this.textoDeRegistro="Usuario Registrado";
+   }else{
+    this.textoDeRegistro="Usuario no registrado";
+
+   }
+  }
+
+ 
   
 }
